078a2a360c9b0c95f1deb69d952a891db31c9f13
23karim <karim.kekli@esprit.tn>
2026-03-23 00:13:00 +0100

    Ajout : Middleware Multer et configuration Cloudinary pour la gestion des images

---
+++ middlewares/cloudinary.js
@@ -0,0 +1,8 @@
+import {v2 as cloudinary} from 'cloudinary';
+          
+cloudinary.config({ 
+  cloud_name: 'dwkqcg27p', 
+  api_key: '141757985526898', 
+  api_secret: 'wntXGJ_MNhu4qyVzNY1QWTelee0' 
+});
+export default cloudinary;
